1d2c4241d96793858c7e4272be535de90ac2018a
Elias Fatsi <efatsi@comcast.net>
2012-07-18 00:02:41 -0600

    first commit, readme updated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AutostitchTutorial::Application.config.secret_token = 'fb5397ae9ed67cc6963debe235ae0c75e76fa7fc68c903a6bc1d73f7476399263c117e667f56ea1709d9ce44cfc7d02aace04b46c356bcc99c9b67a084904c05'
